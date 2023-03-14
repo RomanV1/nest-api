66f5c2bf9337c46044cd24235fdd7dbe0ec938a4
Roma <gnida2404@yandex.ru>
2023-03-14 19:45:32 +0300

    feat: added validation decorators

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,5 +1,19 @@
-export interface CreateUserDto {
+import { IsEmail, IsString, Length, MinLength } from 'class-validator'
+import { PartialType } from '@nestjs/mapped-types'
+
+export class CreateUserDto {
+    @IsString()
+    @Length(3, 20)
     login: string
+
+    @IsString()
+    @IsEmail()
+    @MinLength(5)
     email: string
-    hash: string
+
+    @IsString()
+    @MinLength(8)
+    password: string
 }
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
