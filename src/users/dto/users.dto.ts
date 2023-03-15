8e7d46de7fc906bcf06998b476da6bfe0e2f566b
Roma <gnida2404@yandex.ru>
2023-03-15 17:07:26 +0300

    feat: added new validation

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,17 +1,20 @@
-import { IsEmail, IsString, Length, MinLength } from 'class-validator'
+import { IsDefined, IsEmail, IsString, Length, MinLength } from 'class-validator'
 import { PartialType } from '@nestjs/mapped-types'
 
 export class CreateUserDto {
     @IsString()
+    @IsDefined()
     @Length(3, 20)
     login: string
 
     @IsString()
+    @IsDefined()
     @IsEmail()
     @MinLength(5)
     email: string
 
     @IsString()
+    @IsDefined()
     @MinLength(8)
     password: string
 }
