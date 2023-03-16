84902e2842c82b8391964e2864e87acb0184505f
Roma <gnida2404@yandex.ru>
2023-03-16 18:30:57 +0300

    feat: added Swagger

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,18 +1,28 @@
 import { IsDefined, IsEmail, IsString, Length, MinLength } from 'class-validator'
-import { PartialType } from '@nestjs/mapped-types'
+// import { PartialType } from '@nestjs/mapped-types'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
 
 export class CreateUserDto {
+    @ApiProperty({
+        default: 'user',
+    })
     @IsString()
     @IsDefined()
     @Length(3, 20)
     login: string
 
+    @ApiProperty({
+        default: 'user@gmail.com',
+    })
     @IsString()
     @IsDefined()
     @IsEmail()
     @MinLength(5)
     email: string
 
+    @ApiProperty({
+        default: '12345678',
+    })
     @IsString()
     @IsDefined()
     @MinLength(8)
