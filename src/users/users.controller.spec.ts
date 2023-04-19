1b5575a51588c547bd8e7b23222907baf07f708f
Roma <gnida2404@yandex.ru>
2023-04-19 19:16:02 +0300

    test: added unit tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,164 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { plainToInstance } from 'class-transformer';
+import { User } from '@prisma/client';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { BaseUserResponse, UpdateUserDto } from './dto/users.dto';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+    let usersController: UsersController;
+    let usersService: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [UsersController],
+            providers: [UsersService, PrismaService],
+        }).compile();
 
-    controller = module.get<UsersController>(UsersController);
-  });
+        usersController = module.get<UsersController>(UsersController);
+        usersService = module.get<UsersService>(UsersService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(usersController).toBeDefined();
+        expect(usersService).toBeDefined();
+    });
+
+    describe('getUsers', () => {
+        const users: User[] = [
+            { id: 1, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() },
+            { id: 2, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() },
+        ];
+
+        it('should return all users', async () => {
+            jest.spyOn(usersService, 'getUsers').mockResolvedValue(users);
+
+            const result = await usersController.getUsers();
+            expect(result).toEqual(users);
+        });
+
+        it('should return 404 not found', async () => {
+            jest.spyOn(usersService, 'getUsers').mockResolvedValue([]);
+
+            try {
+                await usersController.getUsers();
+            } catch (err) {
+                expect(err).toBeInstanceOf(NotFoundException);
+                expect(err.message).toBe('Users is not found');
+            }
+        });
+    });
+
+    describe('createUser', () => {
+        const createdUser: User = { id: 1, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+        it('should return created user', async () => {
+            jest.spyOn(usersService, 'isUserExist').mockResolvedValue(false);
+            jest.spyOn(usersService, 'createUser').mockResolvedValue(createdUser);
+
+            const result = await usersController.createUser(createdUser);
+            expect(result).toEqual(plainToInstance(BaseUserResponse, { message: 'User has been created', user: createdUser }));
+        });
+
+        it('should return 400 bad request', async () => {
+            jest.spyOn(usersService, 'isUserExist').mockResolvedValue(true);
+
+            try {
+                await usersController.createUser(createdUser);
+            } catch (err) {
+                expect(err).toBeInstanceOf(BadRequestException);
+                expect(err.message).toBe('User already exist. Change your login or email');
+            }
+        });
+    });
+
+    describe('getUserById', () => {
+        const user: User = { id: 1, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+        it('should return user', async () => {
+            jest.spyOn(usersService, 'getUserById').mockResolvedValue(user);
+
+            const result = await usersController.getUserById(1);
+            expect(result).toEqual(user);
+        });
+
+        it('should return 404 not found', async () => {
+            jest.spyOn(usersService, 'getUserById').mockResolvedValue(null);
+
+            try {
+                await usersController.getUserById(1);
+            } catch (err) {
+                expect(err).toBeInstanceOf(NotFoundException);
+                expect(err.message).toBe('User is not found');
+            }
+        });
+    });
+
+    describe('deleteUser', () => {
+        const deletedUser: User = { id: 1, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+        it('should return deleted user', async () => {
+            jest.spyOn(usersService, 'getUserById').mockResolvedValue(deletedUser);
+            jest.spyOn(usersService, 'deleteUser').mockResolvedValue(deletedUser);
+
+            const result = await usersController.deleteUser(1);
+            expect(result).toEqual(plainToInstance(BaseUserResponse, { message: 'User has been deleted', user: deletedUser }));
+        });
+
+        it('should return 404 not found', async () => {
+            jest.spyOn(usersService, 'getUserById').mockResolvedValue(null);
+
+            try {
+                await usersController.deleteUser(1);
+            } catch (err) {
+                expect(err).toBeInstanceOf(NotFoundException);
+                expect(err.message).toBe('User is not found');
+            }
+        });
+    });
+
+    describe('updateUser', () => {
+        const user: User = { id: 1, email: 'asd@gmail.com', login: 'asd', password: 'asdji1n', createdAt: new Date() };
+        const body: UpdateUserDto = { login: 'roma', email: 'roma@gmail.com' };
+        const updatedUser: User = { ...user, login: body.login, email: body.email };
+
+        it('should return updated user', async () => {
+            jest.spyOn(usersService, 'getUserById').mockResolvedValue(user);
+            jest.spyOn(usersService, 'updateUser').mockResolvedValue(updatedUser);
+
+            const result = await usersController.updateUser(1, body);
+            expect(result).toEqual(plainToInstance(BaseUserResponse, { message: 'User has been updated', user: updatedUser }));
+        });
+
+        it('should return 404 not found', async () => {
+            jest.spyOn(usersService, 'getUserById').mockResolvedValue(null);
+
+            try {
+                await usersController.updateUser(1, user);
+            } catch (err) {
+                expect(err).toBeInstanceOf(NotFoundException);
+                expect(err.message).toBe('User is not found');
+            }
+        });
+
+        it('should return 400 bad request: Empty body', async () => {
+            try {
+                await usersController.updateUser(1, {});
+            } catch (err) {
+                expect(err).toBeInstanceOf(BadRequestException);
+                expect(err.message).toBe('Request body must not be empty');
+            }
+        });
+
+        it('should return updated user with new hash', async () => {
+            jest.spyOn(usersService, 'getUserById').mockResolvedValue(user);
+            jest.spyOn(usersService, 'createHash').mockResolvedValue('1111111');
+            jest.spyOn(usersService, 'updateUser').mockResolvedValue({ ...updatedUser, password: '1111111' });
+
+            const result = await usersController.updateUser(1, body);
+            expect(result).toEqual(plainToInstance(BaseUserResponse, { message: 'User has been updated', user: { ...updatedUser, password: '1111111' } }));
+        });
+    });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,229 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { UpdateUserDto } from './dto/users.dto';
 
 describe('UsersService', () => {
-  let service: UsersService;
+    let usersService: UsersService;
+    let prismaService: PrismaService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [UsersService, PrismaService],
+        }).compile();
 
-    service = module.get<UsersService>(UsersService);
-  });
+        usersService = module.get<UsersService>(UsersService);
+        prismaService = module.get<PrismaService>(PrismaService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(usersService).toBeDefined();
+        expect(prismaService).toBeDefined();
+    });
+
+    describe('getUsers', () => {
+        it('should return an array of users', async () => {
+            const mockUsers: User[] = [
+                { id: 1, email: '123@gmail.com', login: '321', password: '123123', createdAt: new Date() },
+                { id: 2, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() },
+            ];
+
+            jest.spyOn(prismaService.user, 'findMany').mockResolvedValueOnce(mockUsers);
+
+            const users = await usersService.getUsers();
+
+            expect(users).toEqual(mockUsers);
+        });
+
+        it('should return error', async () => {
+            const eMessage = 'some error';
+
+            jest.spyOn(prismaService.user, 'findMany').mockRejectedValueOnce(new Error(eMessage));
+
+            try {
+                await usersService.getUsers();
+            } catch (e) {
+                expect(e.message).toEqual(`class UsersService method getUsers \n${eMessage}`);
+            }
+        });
+
+        describe('createUser', () => {
+            const mockUser: User = { id: 2, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+            it('should return created user', async () => {
+                jest.spyOn(prismaService.user, 'create').mockResolvedValueOnce(mockUser);
+
+                const users = await usersService.createUser(mockUser);
+
+                expect(users).toEqual(mockUser);
+            });
+
+            it('should return error', async () => {
+                const eMessage = 'some error';
+
+                jest.spyOn(prismaService.user, 'create').mockRejectedValueOnce(new Error(eMessage));
+
+                try {
+                    await usersService.createUser(mockUser);
+                } catch (e) {
+                    expect(e.message).toEqual(`class UsersService method createUser \n${eMessage}`);
+                }
+            });
+        });
+
+        describe('createHash', () => {
+            const password: string = '1234567890';
+
+            it('should return a hashed password', async () => {
+                jest.spyOn(bcrypt, 'hash').mockResolvedValueOnce('sad');
+
+                const hash = await usersService.createHash(password);
+
+                expect(hash).not.toEqual(password);
+            });
+
+            it('should return error', async () => {
+                const eMessage = 'some error';
+
+                jest.spyOn(bcrypt, 'hash').mockRejectedValueOnce(new Error(eMessage));
+
+                try {
+                    await usersService.createHash(password);
+                } catch (e) {
+                    expect(e.message).toEqual(`class UsersService method createHash \n${eMessage}`);
+                }
+            });
+        });
+
+        describe('isUserExist', () => {
+            const mockUser: User = { id: 2, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+            it('should find user and return true', async () => {
+                jest.spyOn(prismaService.user, 'findFirst').mockResolvedValueOnce(mockUser);
+
+                const user = await usersService.isUserExist(mockUser);
+
+                expect(user).toEqual(true);
+            });
+
+            it('should return false', async () => {
+                jest.spyOn(prismaService.user, 'findFirst').mockResolvedValueOnce(null);
+
+                const user = await usersService.isUserExist(mockUser);
+
+                expect(user).toEqual(false);
+            });
+
+            it('should return error', async () => {
+                const eMessage = 'some error';
+
+                jest.spyOn(prismaService.user, 'findFirst').mockRejectedValueOnce(new Error(eMessage));
+
+                try {
+                    await usersService.isUserExist(mockUser);
+                } catch (e) {
+                    expect(e.message).toEqual(`class UsersService method isUserExist \n${eMessage}`);
+                }
+            });
+        });
+
+        describe('getUserById', () => {
+            const mockUser: User = { id: 2, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+            it('should find user', async () => {
+                jest.spyOn(prismaService.user, 'findFirst').mockResolvedValueOnce(mockUser);
+
+                const user = await usersService.getUserById(2);
+
+                expect(user).toEqual(mockUser);
+            });
+
+            it('should return null', async () => {
+                jest.spyOn(prismaService.user, 'findFirst').mockResolvedValueOnce(null);
+
+                const user = await usersService.getUserById(2);
+
+                expect(user).toEqual(null);
+            });
+
+            it('should return error', async () => {
+                const eMessage = 'some error';
+
+                jest.spyOn(prismaService.user, 'findFirst').mockRejectedValueOnce(new Error(eMessage));
+
+                try {
+                    await usersService.getUserById(2);
+                } catch (e) {
+                    expect(e.message).toEqual(`class UsersService method getUserById \n${eMessage}`);
+                }
+            });
+        });
+
+        describe('deleteUser', () => {
+            const mockUser: User = { id: 2, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+            it('should delete user', async () => {
+                jest.spyOn(prismaService.user, 'delete').mockResolvedValueOnce(mockUser);
+
+                const user = await usersService.deleteUser(2);
+
+                expect(user).toEqual(mockUser);
+            });
+
+            it('should return null', async () => {
+                jest.spyOn(prismaService.user, 'delete').mockResolvedValueOnce(null);
+
+                const user = await usersService.deleteUser(2);
+
+                expect(user).toEqual(null);
+            });
+
+            it('should return error', async () => {
+                const eMessage = 'some error';
+
+                jest.spyOn(prismaService.user, 'delete').mockRejectedValueOnce(new Error(eMessage));
+
+                try {
+                    await usersService.deleteUser(2);
+                } catch (e) {
+                    expect(e.message).toEqual(`class UsersService method deleteUser \n${eMessage}`);
+                }
+            });
+        });
+
+        describe('updateUser', () => {
+            const mockUser: UpdateUserDto = { email: 'asd@gmail.com', login: '123' };
+            const mockUpdatedUser: User = { id: 2, email: 'asd@gmail.com', login: '123', password: 'asdji1n', createdAt: new Date() };
+
+            it('should return updated user', async () => {
+                jest.spyOn(prismaService.user, 'update').mockResolvedValueOnce(mockUpdatedUser);
+
+                const user = await usersService.updateUser(2, mockUser);
+
+                expect(user).toEqual(mockUpdatedUser);
+            });
+
+            it('should return null', async () => {
+                jest.spyOn(prismaService.user, 'update').mockResolvedValueOnce(null);
+
+                const user = await usersService.updateUser(2, mockUser);
+
+                expect(user).toEqual(null);
+            });
+
+            it('should return error', async () => {
+                const eMessage = 'some error';
+
+                jest.spyOn(prismaService.user, 'update').mockRejectedValueOnce(new Error(eMessage));
+
+                try {
+                    await usersService.updateUser(2, mockUser);
+                } catch (e) {
+                    expect(e.message).toEqual(`class UsersService method updateUser \n${eMessage}`);
+                }
+            });
+        });
+    });
 });
